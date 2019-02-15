36ba4d793536d4b9b0e2cbcc6e56d2c3712bb6c9
Smitha R <41184423+smithasmi95@users.noreply.github.com>
2019-02-15 15:18:31 +0530

    Update TrainerdetailsRepository.cs

--- Repository/Repository/TrainerdetailsRepository.cs
+++ Repository/Repository/TrainerdetailsRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -11,7 +11,7 @@ using TrainingCalendarRepository.Repository.Abstract;
 
 namespace TrainingCalendarRepository.Repository
 {
-   public class TrainerdetailsRepository:ITrainerdetails
+   public class TrainerdetailsRepository: ITrainerdetails
     {
         private readonly Training_CalendarEntities _db;
         public TrainerdetailsRepository()
@@ -24,18 +24,14 @@ namespace TrainingCalendarRepository.Repository
             
             try
             {
-                int courseId = Convert.ToInt32((from course in _db.CourseDetails
-                                                where course.Course_ID == trainerDetail.Course_ID
-                                                select course.Course_ID).FirstOrDefault());
-
+                
                 var CreateTrainerDetails = new TrainerDetail
                 {
-                    Trainer_ID = trainerDetail.Trainer_ID,
-                    //Course_ID = courseId,
-                    User_ID = trainerDetail.User_ID,
+                    Description = trainerDetail.Description,
+                    User_ID = trainerDetail.User_ID, 
                     Trainer_Type = trainerDetail.Trainer_Type,
                     Trainer_Name = trainerDetail.Trainer_Name,
-                    Created_By = trainerDetail.Created_By,
+                    Created_By = "Admin",
                     Created_On = DateTime.Now
                 };
                 _db.TrainerDetails.Add(CreateTrainerDetails);
@@ -67,10 +63,12 @@ namespace TrainingCalendarRepository.Repository
         {
             try
             {
+
                 return (from u in _db.TrainerDetails
                         select new Trainerdetails
                         {
                             Trainer_ID = u.Trainer_ID,
+                            Description = u.Description,
                             User_ID = u.User_ID,
                             Trainer_Type = u.Trainer_Type,
                             Trainer_Name = u.Trainer_Name,
@@ -88,30 +86,69 @@ namespace TrainingCalendarRepository.Repository
         {
             try
             {
-              TrainerDetail e = _db.TrainerDetails.FirstOrDefault(u => u.Trainer_ID == trainerDetails.Trainer_ID);
+              //TrainerDetail e = _db.TrainerDetails.FirstOrDefault(u => u.Trainer_ID == trainerDetails.Trainer_ID);
 
-                //int courseId = Convert.ToInt32((from course in _db.CourseDetails
-                //                                where course.Course_ID == trainerDetails.Course_ID
-                //                                select course.Course_ID).FirstOrDefault());
-                var result = from u in _db.TrainerDetails
-                             where u.Trainer_ID == trainerDetails.Trainer_ID
-                             select u;
-                foreach(TrainerDetail u in result)
+              //  var result = from u in _db.TrainerDetails
+              //               where u.Trainer_ID == trainerDetails.Trainer_ID
+              //               select u;
+              //  foreach(TrainerDetail u in result)
+              //  {
+              //      u.Description = trainerDetails.Description;
+              //      u.Trainer_Type = trainerDetails.Trainer_Type;
+              //      u.Trainer_Name = trainerDetails.Trainer_Name;
+              //      //u.Created_By = "Admin";
+              //      u.Updated_On = DateTime.Now;
+              //  };
+              //  _db.SaveChanges();
+              //  return true;
+                var result = _db.TrainerDetails.FirstOrDefault(r => r.Trainer_ID == trainerDetails.Trainer_ID);
+                if (result != null)
                 {
-                    //u.Course_ID = courseId;
-                    u.Trainer_Type = trainerDetails.Trainer_Type;
-                    u.Trainer_Name = trainerDetails.Trainer_Name;
-                    u.Created_By = trainerDetails.Created_By;
-                    u.Updated_On = DateTime.Now;
-                };
+                    result.Trainer_ID = trainerDetails.Trainer_ID;
+                    result.Trainer_Name=trainerDetails.Trainer_Name;
+                    result.Trainer_Type = trainerDetails.Trainer_Type;
+                    result.Updated_On = DateTime.Now;
+                    result.Description = trainerDetails.Description;
+                }
                 _db.SaveChanges();
                 return true;
-
             }
             catch(Exception ex)
             {
                 throw ex;
             }
         }
+        public IEnumerable GetTrainerById(Trainerdetails trainerDetails)
+        {
+            try
+            {
+                var result = from u in _db.TrainerDetails
+                             where u.Trainer_ID == trainerDetails.Trainer_ID
+                             select new Trainerdetails
+
+                             {
+                                 Trainer_ID = u.Trainer_ID,
+                                 Description = u.Description,
+                                  User_ID = u.User_ID,
+                                 Trainer_Type = u.Trainer_Type,
+                                 Trainer_Name = u.Trainer_Name,
+                                 Created_By = u.Created_By,
+                                 Created_On = u.Created_On,
+                                 Updated_On = u.Updated_On
+                             };
+            if (result.Count() > 0)
+            {
+                return result;
+            }
+            else
+            {
+                return result = null;
+            }
+        }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+}
     }
 }
