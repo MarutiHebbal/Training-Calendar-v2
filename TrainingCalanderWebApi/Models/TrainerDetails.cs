01c212bb7bcd72fe5a9ffc552e92f3bbedd973fc
Smitha R <41184423+smithasmi95@users.noreply.github.com>
2019-02-15 15:14:54 +0530

    Update TrainerDetails.cs

--- TrainingCalanderWebApi/Models/TrainerDetails.cs
+++ TrainingCalanderWebApi/Models/TrainerDetails.cs
@@ -7,13 +7,20 @@ namespace TrainingCalendarWebAPI.Models
 {
     public class TrainerDetails
     {
-        public decimal Trainer_ID { get; set; }
-        public decimal Course_ID { get; set; }
+       public decimal Trainer_ID { get; set; }
+
         public Nullable<decimal> User_ID { get; set; }
+
         public bool Trainer_Type { get; set; }
+
         public string Trainer_Name { get; set; }
+
         public string Created_By { get; set; }
-        public System.DateTime Created_On { get; set; }
+
+        public string Created_On { get; set; }
+
         public Nullable<System.DateTime> Updated_On { get; set; }
+
+        public string Description { get; set; }
     }
 }
